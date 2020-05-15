171477f588fa349841cad43997296d5e705d166c
Karandeep Singh <karandeep.singh@nagarro.com>
2020-05-15 08:44:11 +0530

    Add secret json

--- UsersAPI/Program.cs
+++ UsersAPI/Program.cs
@@ -18,6 +18,12 @@ namespace UsersAPI
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+              config.AddJsonFile("secrets/appsettings.secrets.json",
+                  optional: true,
+                  reloadOnChange: true);
+            })
             .ConfigureWebHostDefaults(webBuilder =>
             {
               webBuilder.UseStartup<Startup>();
